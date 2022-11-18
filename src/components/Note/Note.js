e0b3dfb1751ad8d4d2148728ebb1093c588a9a55
Pedro Sanchez Lampolia <p.sanchez.lampolia@viseven.com>
2022-11-18 09:41:19 -0300

    confirm delete modal

--- src/components/Note/Note.js
+++ src/components/Note/Note.js
@@ -1,9 +1,11 @@
+import { useState } from 'react';
 import { useNote } from '../NoteLayout/NoteLayout';
 import { Link, useNavigate } from 'react-router-dom';
-import { Row, Col, Stack, Badge, Button } from 'react-bootstrap';
+import { Row, Col, Stack, Badge, Button, Modal } from 'react-bootstrap';
 import ReactMarkdown from 'react-markdown';
 
 export default function Note({ onDelete }) {
+  const [deleteNoteModalIsOpen, setDeleteNoteModalIsOpen] = useState(false);
   const note = useNote();
   const navigate = useNavigate();
 
@@ -31,8 +33,9 @@ export default function Note({ onDelete }) {
             </Link>
             <Button
               onClick={() => {
-                onDelete(note.id);
-                navigate('/');
+                // onDelete(note.id);
+                // navigate('/');
+                setDeleteNoteModalIsOpen(true);
               }}
               variant="outline-danger"
             >
@@ -47,6 +50,40 @@ export default function Note({ onDelete }) {
         </Col>
       </Row>
       <ReactMarkdown className="note-markdown">{note.markdown}</ReactMarkdown>
+
+      <DeleteNoteModal
+        onDelete={onDelete}
+        noteId={note.id}
+        navigate={navigate}
+        show={deleteNoteModalIsOpen}
+        handleClose={() => setDeleteNoteModalIsOpen(false)}
+      />
     </>
+  );
+}
+
+function DeleteNoteModal({ onDelete, show, handleClose, noteId, navigate }) {
+  return (
+    <Modal className="modal" show={show} onHide={handleClose}>
+      <Modal.Header closeButton>
+        <Modal.Title>Delete note?</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Stack className="justify-content-end" direction="horizontal" gap={2}>
+          <Button
+            onClick={() => {
+              onDelete(noteId);
+              navigate('/');
+            }}
+            variant="outline-danger"
+          >
+            Yes
+          </Button>
+          <Button onClick={handleClose} variant="outline-secondary">
+            No
+          </Button>
+        </Stack>
+      </Modal.Body>
+    </Modal>
   );
 }
