9dff284fb6ec59e9e0152edd99dc6e0c41eac8ad
Pedro Sanchez Lampolia <p.sanchez.lampolia@viseven.com>
2022-12-14 11:32:31 -0300

    md styles + copy btn

--- src/components/Note/Note.js
+++ src/components/Note/Note.js
@@ -3,12 +3,21 @@ import { useNote } from '../NoteLayout/NoteLayout';
 import { Link, useNavigate } from 'react-router-dom';
 import { Row, Col, Stack, Badge, Button, Modal, Form } from 'react-bootstrap';
 import ReactMarkdown from 'react-markdown';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { oneDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
 import { getPass } from '../../services/firestore/firebase.js';
 
 export default function Note({ onDelete, isLoggedIn }) {
   const [deleteNoteModalIsOpen, setDeleteNoteModalIsOpen] = useState(false);
   const note = useNote();
   const navigate = useNavigate();
+
+  const Pre = ({ children }) => (
+    <pre className="blog-pre">
+      <CodeCopyBtn>{children}</CodeCopyBtn>
+      {children}
+    </pre>
+  );
 
   useEffect(() => {
     document.title = `${note.title} - Peter's Notes`;
@@ -62,7 +71,39 @@ export default function Note({ onDelete, isLoggedIn }) {
           </Stack>
         </Col>
       </Row>
-      <ReactMarkdown className="note-markdown">{note.markdown}</ReactMarkdown>
+      {/* <div className="markdown-container">
+        <Button
+          onClick={() => copyToClipboard(note.markdown)}
+          className="markdown-copy-btn"
+          variant="outline-secondary"
+        >
+          Copy
+        </Button> */}
+      <ReactMarkdown
+        className="note-markdown"
+        components={{
+          pre: Pre,
+          code({ node, inline, className, children, ...props }) {
+            const match = /language-(\w+)/.exec(className || '');
+            return !inline && match ? (
+              <SyntaxHighlighter
+                children={String(children).replace(/\n$/, '')}
+                style={oneDark}
+                language={match[1]}
+                PreTag="div"
+                {...props}
+              />
+            ) : (
+              <code className={className} {...props}>
+                {children}
+              </code>
+            );
+          },
+        }}
+      >
+        {note.markdown}
+      </ReactMarkdown>
+      {/* </div> */}
 
       <DeleteNoteModal
         onDelete={onDelete}
@@ -120,5 +161,46 @@ function DeleteNoteModal({ onDelete, show, handleClose, noteId, navigate }) {
         </Row>
       </Modal.Body>
     </Modal>
+  );
+}
+
+// function copyToClipboard(text) {
+//   let newText = text.substring(4);
+//   newText = newText.slice(0, -3);
+//   navigator.clipboard.writeText(newText);
+// }
+
+function CodeCopyBtn({ children }) {
+  const handleClick = (e) => {
+    navigator.clipboard.writeText(children[0].props.children[0]);
+    console.log(children);
+  };
+
+  return (
+    <div className="code-copy-btn">
+      <svg
+        onClick={handleClick}
+        id="svg5612"
+        viewBox="0 0 128 128"
+        version="1.1"
+      >
+        <g id="layer1" transform="translate(0 -924.36)">
+          <g id="g6783" transform="matrix(.99829 0 0 .99829 -101.01 570.97)">
+            <path
+              id="rect6587"
+              d="m134.94 354c-3.324 0-6 2.676-6 6v96c0 3.324 2.676 6 6 6h82c3.324 0 6-2.676 6-6v-62h-40c-3.324 0-6-2.676-6-6v-34h-42z"
+            />
+            <path
+              id="rect6587-3-9-6-6"
+              d="m113.66 374.22c-3.324 0-6 2.676-6 6v96c0 3.324 2.676 6 6 6h82c3.324 0 6-2.676 6-6v-7h-73c-3.324 0-6-2.676-6-6v-89h-9z"
+            />
+            <path
+              id="rect6587-3-2-9-2"
+              d="m180.94 354v30c0 3.324 2.676 6 6 6h36z"
+            />
+          </g>
+        </g>
+      </svg>
+    </div>
   );
 }
