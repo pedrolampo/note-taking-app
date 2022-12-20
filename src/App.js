a3cac8103cec0345674bfba57ce5bfe001e99a2a
Pedro Sanchez Lampolia <p.sanchez.lampolia@viseven.com>
2022-12-20 10:55:21 -0300

    notes sorted

--- src/App.js
+++ src/App.js
@@ -41,12 +41,14 @@ function App() {
   }, []);
 
   const notesWithTags = useMemo(() => {
-    return notes.map((note) => {
-      return {
-        ...note,
-        tags: tags.filter((tag) => note.tagIds.includes(tag.id)),
-      };
-    });
+    return notes
+      .map((note) => {
+        return {
+          ...note,
+          tags: tags.filter((tag) => note.tagIds.includes(tag.id)),
+        };
+      })
+      .sort((a, b) => a.title.localeCompare(b.title));
   }, [notes, tags]);
 
   function onCreateNote({ tags, ...data }) {
