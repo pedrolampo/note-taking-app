0750a0a83f8f646558017749a3a2520e6d487034
Pedro Sanchez <pedrolampo50@gmail.com>
2023-07-04 14:46:12 -0300

    new overview func

--- src/components/NoteForm/NoteForm.js
+++ src/components/NoteForm/NoteForm.js
@@ -1,9 +1,13 @@
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Form, Stack, Row, Col, Button } from 'react-bootstrap';
 import { Link, useNavigate } from 'react-router-dom';
 import CreatableReactSelect from 'react-select/creatable';
 import { v4 as uuidV4 } from 'uuid';
 import { getUserData } from '../../utils/getUserData';
+import { ReactMarkdown } from 'react-markdown/lib/react-markdown';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { oneDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import remarkGfm from 'remark-gfm';
 
 export default function NoteForm({
   onSubmit,
@@ -18,6 +22,8 @@ export default function NoteForm({
   const titleRef = useRef(null);
   const markdownRef = useRef(null);
   const [selectedTags, setSelectedTags] = useState(tags);
+  const [noteMd, setNoteMd] = useState('');
+  const [showOverview, setShowOverview] = useState(false);
   const navigate = useNavigate();
 
   const selectStyles = {
@@ -54,6 +60,17 @@ export default function NoteForm({
       color: '#fff',
     }),
   };
+
+  useEffect(() => {
+    setNoteMd(markdown);
+  }, [markdown]);
+
+  const Pre = ({ children }) => (
+    <pre className="blog-pre">
+      <CodeCopyBtn>{children}</CodeCopyBtn>
+      {children}
+    </pre>
+  );
 
   function checkIfOwner(owner) {
     if (window.location.href.includes('/new')) return true;
@@ -113,14 +130,52 @@ export default function NoteForm({
           </Col>
         </Row>
         <Form.Group controlId="markdown">
-          <Form.Label>Body</Form.Label>
-          <Form.Control
-            ref={markdownRef}
-            defaultValue={markdown}
-            required
-            as="textarea"
-            rows={15}
-          />
+          <div className="edit-note-labels">
+            <Form.Label>Body</Form.Label>
+            <Form.Check
+              onChange={(e) => setShowOverview(e.target.checked)}
+              label="Toggle Note Overview"
+              reverse
+            />
+          </div>
+          <div className={showOverview ? 'edit-note-form' : undefined}>
+            <Form.Control
+              ref={markdownRef}
+              onChange={(e) => setNoteMd(e.target.value)}
+              defaultValue={markdown}
+              required
+              as="textarea"
+              rows={15}
+            />
+
+            {showOverview && (
+              <ReactMarkdown
+                className="note-markdown"
+                remarkPlugins={[remarkGfm]}
+                components={{
+                  pre: Pre,
+                  code({ node, inline, className, children, ...props }) {
+                    const match = /language-(\w+)/.exec(className || '');
+                    return !inline && match ? (
+                      <SyntaxHighlighter
+                        children={String(children).replace(/\n$/, '')}
+                        style={oneDark}
+                        language={match[1]}
+                        PreTag="div"
+                        {...props}
+                      />
+                    ) : (
+                      <code className={className} {...props}>
+                        {children}
+                      </code>
+                    );
+                  },
+                }}
+              >
+                {noteMd}
+              </ReactMarkdown>
+            )}
+          </div>
         </Form.Group>
         <Stack direction="horizontal" gap={2} className="justify-content-end">
           {isLoggedIn && checkIfOwner(owner) && (
@@ -145,5 +200,74 @@ export default function NoteForm({
         </Stack>
       </Stack>
     </Form>
+  );
+}
+
+function CodeCopyBtn({ children }) {
+  const handleClick = (e) => {
+    navigator.clipboard.writeText(children[0].props.children[0]);
+
+    if (e.target.classList.contains('code-copy-btn')) {
+      e.target.classList.add('success');
+      e.target.children[0].classList.add('hidden');
+      e.target.children[1].classList.remove('hidden');
+
+      setTimeout(() => {
+        e.target.classList.remove('success');
+        e.target.children[0].classList.remove('hidden');
+        e.target.children[1].classList.add('hidden');
+      }, 2000);
+
+      return;
+    } else if (e.target.classList.contains('octicon')) {
+      e.target.parentNode.classList.add('success');
+      e.target.classList.add('hidden');
+      e.target.nextSibling.classList.remove('hidden');
+
+      setTimeout(() => {
+        e.target.parentNode.classList.remove('success');
+        e.target.classList.remove('hidden');
+        e.target.nextSibling.classList.add('hidden');
+      }, 2000);
+
+      return;
+    } else return;
+  };
+
+  return (
+    <div className="code-copy-btn" onClick={handleClick}>
+      <svg
+        aria-hidden="true"
+        height="16"
+        viewBox="0 0 16 16"
+        version="1.1"
+        width="16"
+        data-view-component="true"
+        className="octicon octicon-copy js-clipboard-copy-icon m-2 copy-icon"
+      >
+        <path
+          fillRule="evenodd"
+          d="M0 6.75C0 5.784.784 5 1.75 5h1.5a.75.75 0 010 1.5h-1.5a.25.25 0 00-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 00.25-.25v-1.5a.75.75 0 011.5 0v1.5A1.75 1.75 0 019.25 16h-7.5A1.75 1.75 0 010 14.25v-7.5z"
+        ></path>
+        <path
+          fillRule="evenodd"
+          d="M5 1.75C5 .784 5.784 0 6.75 0h7.5C15.216 0 16 .784 16 1.75v7.5A1.75 1.75 0 0114.25 11h-7.5A1.75 1.75 0 015 9.25v-7.5zm1.75-.25a.25.25 0 00-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 00.25-.25v-7.5a.25.25 0 00-.25-.25h-7.5z"
+        ></path>
+      </svg>
+      <svg
+        aria-hidden="true"
+        height="16"
+        viewBox="0 0 16 16"
+        version="1.1"
+        width="16"
+        data-view-component="true"
+        className="octicon octicon-check js-clipboard-check-icon color-fg-success m-2 copy-done-icon hidden"
+      >
+        <path
+          fillRule="evenodd"
+          d="M13.78 4.22a.75.75 0 010 1.06l-7.25 7.25a.75.75 0 01-1.06 0L2.22 9.28a.75.75 0 011.06-1.06L6 10.94l6.72-6.72a.75.75 0 011.06 0z"
+        ></path>
+      </svg>
+    </div>
   );
 }
