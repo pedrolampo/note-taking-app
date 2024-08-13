8f75d94fd79a9a540975ed2a8e214d744ca1f190
Pedro Sanchez <pedrolampo50@gmail.com>
2024-08-13 10:48:53 -0300

    user context upd

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -1,16 +1,22 @@
 import React, { useState } from 'react';
+import { searchUsers } from '../services/firestore/firebase';
 const UserContext = React.createContext();
 
 export const UserContextProvider = ({ children }) => {
   const [user, setUser] = useState();
   const [isPowerUser, setIsPowerUser] = useState(false);
 
-  const login = (user) => {
+  const login = async (user) => {
     setUser(user);
+
+    let userData = await searchUsers().then((data) => {
+      return data.find((el) => el.uid === user.uid);
+    });
 
     const safeUser = {
       email: user.email,
       uid: user.uid,
+      name: userData.name,
     };
 
     window.localStorage.setItem('user', JSON.stringify(safeUser));
